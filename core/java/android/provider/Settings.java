2ec31bb34a4c2d040815c1fb3e5860eedeaef7ac
Jmz <jmztaylor@gmail.com>
2014-01-06 15:30:15 -0500

    SystemUI: Qs: Force Expanded Notifications [1/2]
    This will force expanded notifications for all apps that support expanded notifications
    
    Change-Id: I68c76aa37f963cc7f3f93989c6101869e100d5f2
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6784,6 +6784,12 @@ public final class Settings {
         public static final Validator POCKET_JUDGE_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
