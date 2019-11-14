24e8870502fe6f28cef7c0acb79d9deeb6009eba
Anushek Prasal <anushekprasal@gmail.com>
2019-11-14 14:07:44 +0530

    base: Add refresh rate setting [1/2]
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6797,6 +6797,11 @@ public final class Settings {
         private static final Validator AUTO_BRIGHTNESS_MIN_VALUE_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * @hide
+         */
+        public static final String REFRESH_RATE_SETTING = "refresh_rate_setting";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
