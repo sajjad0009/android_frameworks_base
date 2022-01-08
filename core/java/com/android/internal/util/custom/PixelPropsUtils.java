e0f693417a3e5ad9c814345949c2908ece7bfe9e
Pranav Vashi <neobuddy89@gmail.com>
2022-01-08 18:35:43 +0530

    PixelPropsUtils: Do not change props for play store
    * Else Play Protect certification fails.
    
    Test: Open Play Store > Settings > About
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -48,7 +48,6 @@ public class PixelPropsUtils {
     private static final Map<String, ArrayList<String>> propsToKeep;
     private static final String[] extraPackagesToChange = {
             "com.android.chrome",
-            "com.android.vending",
             "com.breel.wallpapers20",
      	    "com.google.android.apps.wallpaper",
             "com.google.android.gm"
