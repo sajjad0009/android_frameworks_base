095ae2e3df82e43c4fb2e883054a09f4b0577483
johnmart19 <johnivan19999@gmail.com>
2022-05-11 13:38:03 +0700

    PixelPropsUtils: Do not change props for Fortnite
    else game crash after logo

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -97,9 +97,7 @@ public class PixelPropsUtils {
             "com.riotgames.league.wildrift",
             "com.riotgames.league.wildrifttw",
             "com.riotgames.league.wildriftvn",
-            "com.netease.lztgglobal",
-            "com.epicgames.fortnite",
-            "com.epicgames.portal"
+            "com.netease.lztgglobal"
     };
 
     // Packages to Spoof as Mi 11 Ultra
